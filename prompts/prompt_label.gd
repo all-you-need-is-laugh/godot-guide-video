2cef912a4c5f8e1a4e72c46dbfd3a5e5d3e55d0f
all-you-need-is-laugh <smerost2@gmail.com>
2026-01-04 14:48:01 +0100

    feat: enhance input prompts for build and walk modes with new actions and contexts

--- prompts/prompt_label.gd
+++ prompts/prompt_label.gd
@@ -2,6 +2,7 @@ extends RichTextLabel
 
 @export_multiline var prompt: String = ""
 @export var actions: Array[GUIDEAction] = []
+@export var show_in_contexts: Array[GUIDEMappingContext] = []
 
 var _formatter: GUIDEInputFormatter = GUIDEInputFormatter.for_active_contexts(64)
 
@@ -10,6 +11,12 @@ func _ready():
 	_update_label()
 
 func _update_label():
+	if not show_in_contexts.any(func(context): return GUIDE.is_mapping_context_enabled(context)):
+		visible = false
+		return
+		
+	visible = true
+	
 	var icons: Array[String] = []
 	
 	for action in actions:
